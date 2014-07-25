0390ad0030f6fda8e39d9459ba8f7d8e225c1528
Jackson Chen <tiger2chen@gmail.com>
2014-07-25 00:32:02 -0600

    Creates Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -39,5 +41,18 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
+  end
+
+  describe "Contact page" do
+
+  	it "should have the content 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact Us')
+  	end
+
+  	it "should have the title 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact Us')
+  	end
   end
 end
